b10d6db64f9909c05a947dbe32834a53057602a2
Harwinder Sekhon <hssekhon93@hotmail.com>
2022-05-11 12:25:03 -0400

    Create hostname.sh

---
+++ hostname.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##only ubuntu
+echo current hostname =  $(hostname)
+echo Enter new host
+read hostinput
+currenthostname=$(hostname)
+newhostname=$hostinput
+echo New hostname will be = $hostinput
+sed -i "s/$currenthostname/$newhostname/g" /etc/hosts
+sed -i "s/$currenthostname/$newhostname/g" /etc/hostname
+echo going to reboot ...
+sleep 5
+exit 0
